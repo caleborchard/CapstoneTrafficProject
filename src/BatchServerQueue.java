05e85f1a09b2e2fe22d0bb52b08375e1b4cd9ab0
Caleb Orchard <caleborchard08@gmail.com>
2025-04-02 22:34:39 -0400

    Fix bug where currentTime is not properly updated in BatchServerQueue's stopAtStation function.

--- src/BatchServerQueue.java
+++ src/BatchServerQueue.java
@@ -1,9 +1,10 @@
 public class BatchServerQueue {
-    private LoopingQueue<Station> stationQueue;
+    private final LoopingQueue<Station> stationQueue;
     private Queue<Job> currentPassengers;
     private Station currentStation;
     private double nextStopTime;
     private VehicleInfo trainInfo;
+    private double distanceFromOriginStation = 0;
 
     public BatchServerQueue(VehicleInfo vehicleInfo, LoopingQueue<Station> stationQueue) {
         this.stationQueue = stationQueue;
@@ -15,20 +16,24 @@ public class BatchServerQueue {
 
     public int passengerCount() { return currentPassengers.getLength(); }
 
-    public void stopAtStation(double currentTime) {
+    public double stopAtStation(double currentTime) {
         currentStation = stationQueue.dequeue();
 
-        currentTime += (currentStation.getDistanceFromOriginStation() / trainInfo.getVehicleSpeed())*60;
+        double stationDistanceFromOrigin = currentStation.getDistanceFromOriginStation();
+        double distanceToTravel = Math.abs(stationDistanceFromOrigin - distanceFromOriginStation);
+        double timeToTravel = (distanceToTravel / trainInfo.getVehicleSpeed())*60;
+        distanceFromOriginStation = stationDistanceFromOrigin;
 
         currentStation.getBusArrivals(currentTime);
         Job deq = currentStation.stationWaiters.dequeue();
         while(deq != null && currentPassengers.length < trainInfo.getVehicleCapacity()) {
             currentPassengers.enqueue(deq);
             deq = currentStation.stationWaiters.dequeue();
-            //System.out.println("Passenger boarded");
         }
-        System.out.println("Number of passengers: " + passengerCount() + ", CurrentTime=" + currentTime);
+        System.out.println("Stopping at " + currentStation.getName() + ". Number of passengers: " + passengerCount() + ", CurrentTime=" + currentTime);
         //TODO: Passenger unloading
+
+        return timeToTravel;
     }
 
     public static UnitTestResult UnitTest() {
@@ -36,15 +41,15 @@ public class BatchServerQueue {
 
         double currentTime = 0.0;
         LoopingQueue<Station> globalStationQueue = new LoopingQueue<Station>();
-        for(int i = 0; i < 10; i++) {
-            globalStationQueue.enqueue(new Station("stationNum" + i, i*50, 1000000, 500000, new VehicleInfo(30, 15, 50)));
+        for(int i = 1; i < 10; i++) {
+            globalStationQueue.enqueue(new Station("stationNum" + i, i*50, 0, 0, new VehicleInfo(30, 15, 50)));
         }
 
         BatchServerQueue train1 = new BatchServerQueue(new VehicleInfo(500, 30, 250), globalStationQueue);
 
-        for(int i = 0; i < 10; i++) {
-            train1.stopAtStation(currentTime);
-            System.out.println(train1.currentStation.getName());
+        for(int i = 0; i < 19; i++) {
+            currentTime += train1.stopAtStation(currentTime);
+            //System.out.println(train1.currentStation.getName());
         }
         return result;
     }
